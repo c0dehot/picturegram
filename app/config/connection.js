d1ece71d88e91ca08a3461b9e7127407221b290b
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-11-09 12:52:45 -0500

    Fixed mongo url

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require( 'mongoose' );
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`,
-    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect(process.env.MONGO_URL ? process.env.MONGO_URL
+    : `mongodb://localhost:27017/${process.env.DB_NAME}`,
+{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 module.exports = mongoose;
