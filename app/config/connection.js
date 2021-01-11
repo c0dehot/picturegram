e236074a921653bf48f8d9e2d1be18d067e5f3b0
Filipe Laborde-Basto <codefreeze8@gmail.com>
2021-01-11 00:57:45 -0500

    Fixed up connection string

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require( 'mongoose' );
 
-mongoose.connect(process.env.MONGODB_URI ? process.env.MONGODB_URI : `mongodb://localhost:27017/${process.env.DB_NAME}`,
-    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.DB_NAME}`
+
+mongoose.connect(MONGODB_URI,
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
 module.exports = mongoose;
